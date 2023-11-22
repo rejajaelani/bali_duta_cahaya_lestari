e46cf57d3c58e2622680f2499f433d30cc2afbf2
rejajaelani <abdulrejazaelani01@gmail.com>
2023-11-22 18:52:38 +0800

    Update | Database

--- bali_duta_cahaya_lestari.sql
+++ bali_duta_cahaya_lestari.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2023 at 05:01 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.24
+-- Generation Time: Nov 22, 2023 at 11:52 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,7 +34,7 @@ CREATE TABLE `tb_akun` (
   `sifat` varchar(50) NOT NULL,
   `created_at` datetime NOT NULL,
   `update_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_akun`
@@ -42,29 +42,9 @@ CREATE TABLE `tb_akun` (
 
 INSERT INTO `tb_akun` (`id_akun`, `nama`, `id_kategori_akun`, `sifat`, `created_at`, `update_at`) VALUES
 ('101', 'Modal', 5, 'Kredit', '2023-11-19 22:08:40', '2023-11-19 22:08:40'),
-('102', 'Prive', 5, 'Debit', '2023-11-19 23:15:46', '2023-11-19 23:15:46');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_alat`
---
-
-CREATE TABLE `tb_alat` (
-  `id` int(11) NOT NULL,
-  `kode` varchar(255) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `update_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tb_alat`
---
-
-INSERT INTO `tb_alat` (`id`, `kode`, `nama`, `created_at`, `update_at`) VALUES
-(3, 'TEST-001', 'Sapu', '2023-10-22 19:18:59', '2023-10-22 19:31:16'),
-(4, 'TEST-002', 'Tong Sampah', '2023-10-22 19:31:32', '2023-10-22 19:31:32');
+('102', 'Prive', 5, 'Debit', '2023-11-19 23:15:46', '2023-11-19 23:15:46'),
+('110', 'Pendapatan Penjualan', 5, 'Kredit', '2023-11-22 00:20:05', '2023-11-22 00:20:05'),
+('111', 'Penjualan', 5, 'Debet', '2023-11-22 00:19:49', '2023-11-22 00:19:49');
 
 -- --------------------------------------------------------
 
@@ -80,15 +60,7 @@ CREATE TABLE `tb_detail_jurnal` (
   `kredit` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `update_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tb_detail_jurnal`
---
-
-INSERT INTO `tb_detail_jurnal` (`id`, `id_jurnal`, `id_akun`, `debet`, `kredit`, `created_at`, `update_at`) VALUES
-('PK-969658', 'TRX-202311191617130001', '102', 1000000, 0, '2023-11-19 23:17:30', '2023-11-19 23:17:30'),
-('PM-666057', 'TRX-202311191609240001', '101', 10000000, 0, '2023-11-19 23:09:50', '2023-11-19 23:09:50');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -104,14 +76,7 @@ CREATE TABLE `tb_detail_trans_keluar` (
   `kredit` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `update_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tb_detail_trans_keluar`
---
-
-INSERT INTO `tb_detail_trans_keluar` (`id`, `id_transaksi_keluar`, `id_akun`, `debet`, `kredit`, `created_at`, `update_at`) VALUES
-('PK-969658', 'TRX-202311191617130001', '102', 1000000, 0, '2023-11-19 23:17:30', '2023-11-19 23:17:30');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -127,14 +92,14 @@ CREATE TABLE `tb_detail_trans_masuk` (
   `kredit` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `update_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_detail_trans_masuk`
 --
 
 INSERT INTO `tb_detail_trans_masuk` (`id`, `id_transaksi_masuk`, `id_akun`, `debet`, `kredit`, `created_at`, `update_at`) VALUES
-('PM-666057', 'TRX-202311191609240001', '101', 10000000, 0, '2023-11-19 23:09:50', '2023-11-19 23:09:50');
+('PM-414905', 'TRX-202311211722140001', '111', 3220000, 0, '2023-11-22 00:28:51', '2023-11-22 00:28:51');
 
 -- --------------------------------------------------------
 
@@ -149,15 +114,7 @@ CREATE TABLE `tb_jurnal` (
   `tgl_jurnal` date NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `update_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tb_jurnal`
---
-
-INSERT INTO `tb_jurnal` (`id_jurnal`, `keterangan`, `type_transaksi`, `tgl_jurnal`, `created_at`, `update_at`) VALUES
-('TRX-202311191609240001', 'Modal Bu Ani', 2, '2023-11-19', '2023-11-19 23:09:57', '2023-11-19 23:27:43'),
-('TRX-202311191617130001', 'Penarikan uang modal', 4, '2023-11-19', '2023-11-19 23:17:37', '2023-11-19 23:17:37');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -170,7 +127,7 @@ CREATE TABLE `tb_kategori_akun` (
   `nama_kategori` varchar(250) NOT NULL,
   `created_at` datetime NOT NULL,
   `update_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_kategori_akun`
@@ -192,14 +149,7 @@ CREATE TABLE `tb_transaksi_keluar` (
   `tgl_trans_keluar` date NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `update_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tb_transaksi_keluar`
---
-
-INSERT INTO `tb_transaksi_keluar` (`id_transaksi_keluar`, `keterangan`, `type_transaksi`, `tgl_trans_keluar`, `created_at`, `update_at`) VALUES
-('TRX-202311191617130001', 'Penarikan uang modal', 4, '2023-11-19', '2023-11-19 23:17:37', '2023-11-19 23:17:37');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -214,14 +164,14 @@ CREATE TABLE `tb_transaksi_masuk` (
   `tgl_trans_masuk` date NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `update_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_transaksi_masuk`
 --
 
 INSERT INTO `tb_transaksi_masuk` (`id_transaksi_masuk`, `keterangan`, `type_transaksi`, `tgl_trans_masuk`, `created_at`, `update_at`) VALUES
-('TRX-202311191609240001', 'Modal Bu Ani', 2, '2023-11-19', '2023-11-19 23:09:57', '2023-11-19 23:27:43');
+('TRX-202311211722140001', 'Pembelian TV', 1, '2023-11-22', '2023-11-22 00:30:28', '2023-11-22 00:32:56');
 
 -- --------------------------------------------------------
 
@@ -240,7 +190,7 @@ CREATE TABLE `tb_user` (
   `foto` text DEFAULT NULL,
   `created_at` date DEFAULT current_timestamp(),
   `updated_at` date DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_user`
@@ -249,7 +199,7 @@ CREATE TABLE `tb_user` (
 INSERT INTO `tb_user` (`id_user`, `nama`, `email`, `username`, `password`, `level`, `status`, `foto`, `created_at`, `updated_at`) VALUES
 (22, 'admin', 'admin@gmail.com', 'admin', '$2y$10$9pXUd0AJ/9w2OOjb9weKmuiKTBBT4/McwqKDlOAtnKHgMOU1j2flC', 1, 1, '65350c97726c4_1697975447.png', '2023-10-22', '2023-10-22'),
 (24, 'Pimpinan', 'pimpinan@gmail.com', 'pimpinan', '$2y$10$37OUwkTpM9Udi8k3HuqVA.VBgX/O4xP3l6o57Yaluv6YJ4k71/A8q', 2, 1, '654dbe273937b_1699593767.jpg', '2023-11-10', '2023-11-10'),
-(25, 'Akunting', 'akunting@gmail.com', 'akunting', '$2y$10$aNhKCSrv7.LW2SzAawN5SeCIz2dOWXJdWgO9aNzlV5S.n7bUz6mTq', 3, 1, '654dbe60e32d2_1699593824.jpg', '2023-11-10', '2023-11-10');
+(25, 'Akunting', 'akunting@gmail.com', 'akunting', '$2y$10$aNhKCSrv7.LW2SzAawN5SeCIz2dOWXJdWgO9aNzlV5S.n7bUz6mTq', 3, 1, '-', '2023-11-10', '2023-11-10');
 
 --
 -- Indexes for dumped tables
@@ -261,12 +211,6 @@ INSERT INTO `tb_user` (`id_user`, `nama`, `email`, `username`, `password`, `leve
 ALTER TABLE `tb_akun`
   ADD PRIMARY KEY (`id_akun`),
   ADD KEY `fk_tb_akun_tb_kategori_akun` (`id_kategori_akun`);
-
---
--- Indexes for table `tb_alat`
---
-ALTER TABLE `tb_alat`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tb_detail_jurnal`
@@ -319,12 +263,6 @@ ALTER TABLE `tb_user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `tb_alat`
---
-ALTER TABLE `tb_alat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tb_kategori_akun`
